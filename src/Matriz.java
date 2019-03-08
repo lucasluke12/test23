626a40609c346e52442a118a3d0a04f92dbf3eb6
lucasluke12 <lucassilvaee1245@gmail.com>
2019-03-07 23:42:30 -0300

    projeto

---
+++ src/Matriz.java
@@ -0,0 +1,40 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Matriz {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner t = new Scanner(System.in);
+        System.out.println("N: ");
+        int n = t.nextInt();
+        
+        int[][] matriz = new int[n][n];
+        for(int i=0; i<n; i++){
+            for(int ii=0; ii<n; ii++){
+                matriz[i][ii] = t.nextInt();
+            }
+        }
+        int negativo = 0;
+        for(int i=0; i<n; i++){
+            
+                System.out.print(matriz[i][i]+", ");
+                
+                
+            
+        }
+      
+        for(int i=0;i<n;i++){
+            for(int ii=0;ii<n;ii++){
+                if(matriz[i][ii] < 0){
+                    negativo++;
+                }
+            }
+        }
+        System.out.println("");
+        System.out.println("Negativos "+negativo);
+        
+        t.close();
+}
+    
+}
